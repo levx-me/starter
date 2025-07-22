8103189caa4970d96413996dd28045867334cf5b
LevX <team@levx.io>
2025-07-23 04:50:50 +0900

    update

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,31 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+--
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt.shiftwidth = 4
+        vim.opt.tabstop = 4
+        vim.opt.softtabstop = 4
+        vim.opt.ignorecase = false
+        vim.opt.mouse = "a"
+    end,
+})
+autocmd("BufWritePost", {
+    pattern = "*.sol",
+    command = "silent! !forge fmt",
+})
+autocmd("BufWritePost", {
+    pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
+    command = "silent! EslintFixAll",
+})
+autocmd("InsertLeave", {
+    pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
+    command = "silent! EslintFixAll",
+})
