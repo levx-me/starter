82676e0a47ae2976f170dc06c2611add823ab0f7
LevX <team@levx.io>
2025-07-23 05:27:23 +0900

    update lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "solidity_ls_nomicfoundation", "eslint", "tailwindcss" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
