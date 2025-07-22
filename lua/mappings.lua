e9e521cfd6e82f643bb1f2ef314c03b50ddd4203
LevX <team@levx.io>
2025-07-23 06:16:14 +0900

    add mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,24 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 map("n", "rw", ":update<cr><esc>")
+map("n", "]c", function()
+  if vim.wo.diff then
+    return "]c"
+  end
+  vim.schedule(function()
+    require("gitsigns").next_hunk()
+  end)
+  return "<Ignore>"
+end, { desc = "Jump to next hunk" })
+map("n", "[c", function()
+  if vim.wo.diff then
+    return "[c"
+  end
+  vim.schedule(function()
+    require("gitsigns").prev_hunk()
+  end)
+  return "<Ignore>"
+end, { desc = "Jump to prev hunk" })
 map("n", "<leader>lf", vim.diagnostic.open_float, { desc = "Floating diagnostic" })
 map("n", "<leader>rh", require("gitsigns").reset_hunk, { desc = "Rest hunk" })
 map("n", "<leader>ph", require("gitsigns").preview_hunk, { desc = "Preview hunk" })
